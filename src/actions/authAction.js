2c1196a55160e98af5a1455c1fde5685f999c342
Malek Chtioui <chtiouimalek001@gmail.com>
2021-09-15 17:20:00 +0100

    commit

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -89,7 +89,7 @@ export const loadUser = () => (dispatch, getState) => {
             payload: res.data
         }))
         .catch(err => {
-            dispatch(returnErrors(err.response.data, err.response.status))
+            dispatch(returnErrors(err.response, err.response))
             dispatch({
                 type: AUTH_ERROR
             })
