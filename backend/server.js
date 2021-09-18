daf57984754a13cae0746cae22b0cd50c7d3fd76
Malek Chtioui <chtiouimalek001@gmail.com>
2021-09-18 14:38:53 +0100

    commit

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use('/dashboard', dashboardRouter);
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use("/sendmail", mailRouter)
-mongoose.connect(db, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useCreateIndex: true});
 
 app.listen(PORT, () =>{
     console.log("Server is running on Port: " + PORT);
