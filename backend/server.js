b698834ebbc8960a63f384e0c57ba9b354049b3b
Malek Chtioui <chtiouimalek001@gmail.com>
2021-09-18 17:18:53 +0100

    s

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use('/dashboard', dashboardRouter);
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/sendmail', mailRouter)
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB has been connected"))
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true,createIndexes:true, useUnifiedTopology: true}).then(() => console.log("MongoDB has been connected"))
 .catch((err) => console.log(err));
 
 
