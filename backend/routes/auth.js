de8f012823fd3bfb178ac9a3dd9c8acf37414f04
Malek Chtioui <chtiouimalek001@gmail.com>
2021-09-18 18:18:35 +0100

    Ss

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const User = require('../Models/userSchema')
+const User = require('../models/userSchema')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
